bd0d82d13514e957c994c45d824ccc86dd3702fd
HyunJiLim-tech <hyunjilim98@gmail.com>
2020-09-13 22:41:29 +0900

    2019 epper

---
+++ 2019/2019/p8.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() { //8¹ø
+	char input, none;
+	int m;
+	int num[2];
+
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++) {
+		scanf("%c", &none);
+		scanf("%c", &input);
+		if (i == 0)
+			num[0] = input - '0';
+		else {
+			switch (input) {
+			case '+':
+				num[0] = num[0] + num[1];
+				break;
+			case '-':
+				num[0] = num[0] - num[1];
+				break;
+			case '*':
+				num[0] = num[0] * num[1];
+				break;
+			case '/':
+				num[0] = num[0] / num[1];
+				break;
+			default:
+				num[1] = input - '0';
+				break;
+			}
+		}
+	}
+	printf("%d", num[0]);
+}
